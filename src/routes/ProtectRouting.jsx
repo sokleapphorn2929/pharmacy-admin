ccf78bf2da8fa032f3296306372c75bded408d50
SOKLEAP <phornzokl3ap@gmail.com>
2026-08-01 16:32:46 +0700

    fix can't login

--- src/routes/ProtectRouting.jsx
+++ src/routes/ProtectRouting.jsx
@@ -1,7 +1,7 @@
 import { Navigate } from "react-router-dom";
 
 export default function ProtectRouting({ children }) {
-  const token = localStorage.getItem("authToken");
+  const token = localStorage.getItem("token");
 
   if (!token) {
     return <Navigate to="/" replace />;
